0299f1caee2a24fceff6d47ba4c8ff338db5317c
Kopreichen <71689273+Kopreichen@users.noreply.github.com>
2020-09-29 15:37:22 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let score = 0
+input.onGesture(Gesture.Shake, function () {
+    score += randint(80, 100)
+    basic.showNumber(score)
+    if (score >= 80 && score <= 84) {
+        basic.showString("A-")
+    } else if (score >= 85 && score <= 89) {
+        basic.showString("A")
+    } else if (score >= 90 && score <= 100) {
+        basic.showString("A+")
+    }
+})
